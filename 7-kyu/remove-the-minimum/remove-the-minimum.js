2d7e2785722920db0f24e8e9fe665f2b95adcf8d
Skye Fulmer <122103934+SkyeFull90@users.noreply.github.com>
2025-03-30 13:52:02 -0600

    Add solution - CodeHub

---
+++ 7-kyu/remove-the-minimum/remove-the-minimum.js
@@ -0,0 +1,15 @@
+function removeSmallest(numbers) {
+  if (numbers.length === 0) {
+    return [];
+  }
+​
+  const minVal = Math.min(...numbers);
+  const minIndex = numbers.indexOf(minVal);
+​
+  return numbers.filter((_, i) => i !== minIndex);
+}
+​
+console.log(removeSmallest([1, 2, 3, 4, 5])); // Output: [2, 3, 4, 5]
+console.log(removeSmallest([5, 3, 2, 1, 4])); // Output: [5, 3, 2, 4]
+console.log(removeSmallest([2, 2, 1, 2, 1])); // Output: [2, 2, 2, 1]
+console.log(removeSmallest([])); // Output: []
